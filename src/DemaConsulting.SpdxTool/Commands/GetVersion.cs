5a682921a9d1e5b594c37ca0a96f1fa63516f926
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-06-04 22:11:51 -0400

    Add 'get-version' command. (#32)
    Co-authored-by: Malcolm Nixon <malcolm.nixon@elbitsystems-us.com>

---
+++ src/DemaConsulting.SpdxTool/Commands/GetVersion.cs
@@ -0,0 +1,100 @@
+﻿using DemaConsulting.SpdxTool.Spdx;
+using YamlDotNet.Core;
+using YamlDotNet.RepresentationModel;
+
+namespace DemaConsulting.SpdxTool.Commands;
+
+/// <summary>
+/// Get Version Command
+/// </summary>
+public class GetVersion : Command
+{
+    /// <summary>
+    /// Singleton instance of this command
+    /// </summary>
+    public static readonly GetVersion Instance = new();
+
+    /// <summary>
+    /// Entry information for this command
+    /// </summary>
+    public static readonly CommandEntry Entry = new(
+        "get-version",
+        "get-version <spdx.json> <id>",
+        "Get the version of an SPDX package.",
+        new[]
+        {
+            "This command gets the version of an SPDX package.",
+            "",
+            "From the command-line this can be used as:",
+            "  spdx-tool get-version <spdx.json> <id>",
+            "",
+            "From a YAML file this can be used as:",
+            "  - command: get-version",
+            "    inputs:",
+            "      spdx: <spdx.json>             # SPDX file name",
+            "      id: <id>                      # Package ID",
+            "      output: <variable>            # Output variable"
+        },
+        Instance);
+
+    /// <summary>
+    /// Private constructor - this is a singleton
+    /// </summary>
+    private GetVersion()
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Run(string[] args)
+    {
+        // Report an error if the number of arguments is not 2
+        if (args.Length != 2)
+            throw new CommandUsageException("'get-version' command missing arguments");
+
+        // Print the version
+        Console.WriteLine(Get(args[0], args[1]));
+    }
+
+    /// <inheritdoc />
+    public override void Run(YamlMappingNode step, Dictionary<string, string> variables)
+    {
+        // Get the step inputs
+        var inputs = GetMapMap(step, "inputs");
+
+        // Get the 'spdx' input
+        var spdxFile = GetMapString(inputs, "spdx", variables) ??
+                       throw new YamlException(step.Start, step.End, "'get-version' command missing 'spdx' input");
+
+        // Get the 'id' input
+        var id = GetMapString(inputs, "id", variables) ??
+                 throw new YamlException(step.Start, step.End, "'get-version' command missing 'id' input");
+
+        // Get the 'output' input
+        var output = GetMapString(inputs, "output", variables) ??
+                     throw new YamlException(step.Start, step.End, "'get-version' command missing 'output' input");
+
+        // Save the version
+        variables[output] = Get(spdxFile, id);
+    }
+
+    /// <summary>
+    /// Get the version of a package in an SPDX document
+    /// </summary>
+    /// <param name="spdxFile">SPDX document file name</param>
+    /// <param name="id">SPDX package ID</param>
+    /// <returns>Version</returns>
+    /// <exception cref="CommandErrorException">on error</exception>
+    public static string Get(string spdxFile, string id)
+    {
+        // Load the document
+        var doc = SpdxHelpers.LoadJsonDocument(spdxFile);
+
+        // Find the package
+        var package = Array.Find(doc.Packages, p => p.Id == id) ??
+                      throw new CommandErrorException($"Package {id} not found in {spdxFile}");
+
+        // Return the version
+        return package.Version ??
+               throw new CommandErrorException($"Package {id} in {spdxFile} has no version");
+    }
+}
---
+++ test/DemaConsulting.SpdxTool.Tests/TestGetVersion.cs
@@ -0,0 +1,147 @@
+﻿namespace DemaConsulting.SpdxTool.Tests;
+
+[TestClass]
+public class TestGetVersion
+{
+    /// <summary>
+    /// SPDX file for finding packages
+    /// </summary>
+    private const string SpdxContents = "{\r\n" +
+                                        "  \"files\": [],\r\n" +
+                                        "  \"packages\": [" +
+                                        "    {\r\n" +
+                                        "      \"SPDXID\": \"SPDXRef-Package-1\",\r\n" +
+                                        "      \"name\": \"Test Package\",\r\n" +
+                                        "      \"versionInfo\": \"1.0.0\",\r\n" +
+                                        "      \"packageFileName\": \"package1.zip\",\r\n" +
+                                        "      \"downloadLocation\": \"https://github.com/demaconsulting/SpdxTool\",\r\n" +
+                                        "      \"licenseConcluded\": \"MIT\"\r\n" +
+                                        "    },\r\n" +
+                                        "    {\r\n" +
+                                        "      \"SPDXID\": \"SPDXRef-Package-2\",\r\n" +
+                                        "      \"name\": \"Another Test Package\",\r\n" +
+                                        "      \"versionInfo\": \"2.0.0\",\r\n" +
+                                        "      \"packageFileName\": \"package2.tar\",\r\n" +
+                                        "      \"downloadLocation\": \"https://github.com/demaconsulting/SpdxModel\",\r\n" +
+                                        "      \"licenseConcluded\": \"MIT\"\r\n" +
+                                        "    }\r\n" +
+                                        "  ],\r\n" +
+                                        "  \"relationships\": [" +
+                                        "    {\r\n" +
+                                        "      \"spdxElementId\": \"SPDXRef-DOCUMENT\",\r\n" +
+                                        "      \"relatedSpdxElement\": \"SPDXRef-Package-1\",\r\n" +
+                                        "      \"relationshipType\": \"DESCRIBES\"\r\n" +
+                                        "    }\r\n" +
+                                        "  ],\r\n" +
+                                        "  \"spdxVersion\": \"SPDX-2.2\",\r\n" +
+                                        "  \"dataLicense\": \"CC0-1.0\",\r\n" +
+                                        "  \"SPDXID\": \"SPDXRef-DOCUMENT\",\r\n" +
+                                        "  \"name\": \"Test Document\",\r\n" +
+                                        "  \"documentNamespace\": \"https://sbom.spdx.org\",\r\n" +
+                                        "  \"creationInfo\": {\r\n" +
+                                        "    \"created\": \"2021-10-01T00:00:00Z\",\r\n" +
+                                        "    \"creators\": [ \"Person: Malcolm Nixon\" ]\r\n" +
+                                        "  },\r\n" +
+                                        "  \"documentDescribes\": [ \"SPDXRef-Package-1\" ]\r\n" +
+                                        "}";
+
+    [TestMethod]
+    public void GetVersionMissingArguments()
+    {
+        // Run the command
+        var exitCode = Runner.Run(
+            out var output,
+            "dotnet",
+            "DemaConsulting.SpdxTool.dll",
+            "get-version");
+
+        // Verify error reported
+        Assert.AreEqual(1, exitCode);
+        Assert.IsTrue(output.Contains("'get-version' command missing arguments"));
+    }
+
+    [TestMethod]
+    public void GetVersionGenerateMissingFile()
+    {
+        // Run the command
+        var exitCode = Runner.Run(
+            out var output,
+            "dotnet",
+            "DemaConsulting.SpdxTool.dll",
+            "get-version",
+            "missing.spdx.json",
+            "SPDXRef-Package");
+
+        // Verify error reported
+        Assert.AreEqual(1, exitCode);
+        Assert.IsTrue(output.Contains("File not found: missing.spdx.json"));
+    }
+
+    [TestMethod]
+    public void GetVersionCommandLine()
+    {
+        try
+        {
+            // Write the SPDX files
+            File.WriteAllText("spdx.json", SpdxContents);
+
+            // Run the command
+            var exitCode = Runner.Run(
+                out var output,
+                "dotnet",
+                "DemaConsulting.SpdxTool.dll",
+                "get-version",
+                "spdx.json",
+                "SPDXRef-Package-2");
+
+            // Verify package ID
+            Assert.AreEqual(0, exitCode);
+            Assert.IsTrue(output.Contains("2.0.0"));
+        }
+        finally
+        {
+            File.Delete("spdx.json");
+        }
+    }
+
+    [TestMethod]
+    public void GetVersionWorkflow()
+    {
+        // Workflow contents
+        const string workflowContents = "steps:\n" +
+                                        "- command: get-version\n" +
+                                        "  inputs:\n" +
+                                        "    spdx: spdx.json\n" +
+                                        "    id: SPDXRef-Package-2\n" +
+                                        "    output: version\n" +
+                                        "" +
+                                        "- command: print\n" +
+                                        "  inputs:\n" +
+                                        "    text:\n" +
+                                        "    - Found version ${{ version }}";
+
+        try
+        {
+            // Write the SPDX files
+            File.WriteAllText("spdx.json", SpdxContents);
+            File.WriteAllText("workflow.yaml", workflowContents);
+
+            // Run the command
+            var exitCode = Runner.Run(
+                out var output,
+                "dotnet",
+                "DemaConsulting.SpdxTool.dll",
+                "run-workflow",
+                "workflow.yaml");
+
+            // Verify package ID
+            Assert.AreEqual(0, exitCode);
+            Assert.IsTrue(output.Contains("Found version 2.0.0"));
+        }
+        finally
+        {
+            File.Delete("spdx.json");
+            File.Delete("workflow.yaml");
+        }
+    }
+}
